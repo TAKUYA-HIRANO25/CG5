cc4d3cf74e93ca8cf726a9de80c61b04b0207bb5
TAKUYA-HIRANO25 <k023g0013@g.neec.ac.jp>
2025-06-05 11:43:30 +0900

    完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -4,8 +4,13 @@
 #include <d3dcompiler.h>
 #include <cassert>
 #include"RootSignature.h"
+#include "PipeLineState.h"
+#include "VertexBuffer.h"
 
 using namespace KamataEngine;
+
+// 関数プロトタイプ
+void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& vs, Shader& ps);
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -25,24 +30,87 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	ID3D12GraphicsCommandList* commandList = dxCommon->GetCommandList();
 
 #pragma region RootSignature作成
-	// 構造体にデータを用意する
-	/* 
-	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
-	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
-	ID3DBlob* signatureBlob = nullptr;
-	ID3DBlob* errorBlog = nullptr;
-	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlog);
-	if (FAILED(hr)) {
-		DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlog->GetBufferPointer()));
-		assert(false);
-	}
-	// バイナリをもとに生成
-	ID3D12RootSignature* rootSignature = nullptr;
-	hr = dxCommon->GetDevice()->CreateRootSignature(0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
-	assert(SUCCEEDED(hr));
-	*/
 	RootSignature rs;
 	rs.Create();
+
+#pragma endregion
+
+#pragma region BlendState
+
+#pragma endregion
+
+#pragma region RasterizeState
+	
+#pragma endregion
+
+#pragma region VertexShaderをCompile
+	Shader vs;
+	vs.LoadDxc(L"Resources/shaders/TestVS.hlsl", L"vs_6_0");
+	assert(vs.GetDxcBlob() != nullptr);
+#pragma endregion
+
+#pragma region PixelShaderをCompile
+
+	Shader ps;
+	ps.LoadDxc(L"Resources/Shaders/TestPS.hlsl", L"ps_6_0");
+	assert(ps.GetDxcBlob() != nullptr);
+
+#pragma endregion
+
+#pragma region PSO(PiplineStateObject)の生成
+	PipelineState pipelineState;
+	SetupPipelineState(pipelineState, rs, vs, ps);
+#pragma endregion
+
+#pragma region InputLyout
+	
+#pragma endregion
+
+#pragma region VertxBufferViewを作成
+	VertexBuffer vb;
+	vb.Create(sizeof(Vector4) * 3, sizeof(Vector4));
+#pragma endregion
+
+#pragma region 頂点リソースにデータを書き込む
+	Vector4* vertexData = nullptr;
+	vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f}; // 左下
+	vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};   // 上
+	vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};  // 右下
+	// 頂点リソースのマップを解除する
+	//vertexResource->Unmap(0, nullptr);
+#pragma endregion
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// コマンドを読む
+		commandList->SetGraphicsRootSignature(rs.Get());     // rootsignatureの設定
+		commandList->SetPipelineState(pipelineState.Get());     // PSOの設定をする
+		commandList->IASetVertexBuffers(0, 1, vb.GetView()); // VBVの設定
+		// トロポジの設定
+		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		// 頂点数、インデックス数、インデックスの開始位置、インデックスのオフセット
+		commandList->DrawInstanced(3, 1, 0, 0);
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+
+	return 0;
+}
+
+void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& vs, Shader& ps) {
 	// InputLayout
 	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
 	inputElementDescs[0].SemanticName = "POSITION";
@@ -52,8 +120,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
 	inputLayoutDesc.pInputElementDescs = inputElementDescs;
 	inputLayoutDesc.NumElements = _countof(inputElementDescs);
-
-#pragma endregion
 
 #pragma region BlendState
 	D3D12_BLEND_DESC blendDesc{};
@@ -67,22 +133,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
 	// 塗りつぶしモードをソリッドにする（ワイヤーフレームならD3D12_FILL_MODE_WIREFRAME）
 	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
-#pragma endregion
-
-#pragma region VertexShaderをCompile
-	
-
-	Shader vs;
-	vs.LoadDxc(L"Resources/shaders/TestVS.hlsl", L"vs_6_0");
-	assert(vs.GetDxcBlob() != nullptr);
-#pragma endregion
-
-#pragma region PixelShaderをCompile
-
-	Shader ps;
-	ps.LoadDxc(L"Resources/Shaders/TestPS.hlsl", L"ps_6_0");
-	assert(ps.GetDxcBlob() != nullptr);
-
 #pragma endregion
 
 #pragma region PSO(PiplineStateObject)の生成
@@ -101,83 +151,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// どのように画面に色を打ち込むのか設定
 	graphicsPipelineStateDesc.SampleDesc.Count = 1;
 	graphicsPipelineStateDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
-	// PSOの生成
-	ID3D12PipelineState* graphicsPipelineState = nullptr;
-	HRESULT hr = dxCommon->GetDevice()->CreateGraphicsPipelineState(&graphicsPipelineStateDesc, IID_PPV_ARGS(&graphicsPipelineState));
-	assert(SUCCEEDED(hr));
+
 #pragma endregion
 
-#pragma region InputLyout
-	// 頂点リソース用のヒープの設定
-	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
-	uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD; // CPUから書き込みヒープ
-	// 頂点リソースの設定
-	D3D12_RESOURCE_DESC vertexResourceDesc{};
-	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER; // バッファ
-	vertexResourceDesc.Width = sizeof(Vector4) * 3;                 // リソースのサイズ
-	// バッファの場合はこれらは1にする
-	vertexResourceDesc.Height = 1;
-	vertexResourceDesc.DepthOrArraySize = 1;
-	vertexResourceDesc.MipLevels = 1;
-	vertexResourceDesc.SampleDesc.Count = 1;
-	// バッファの場合はこれにする決まり
-	vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-	// 実際に頂点リソースを生成する
-	ID3D12Resource* vertexResource = nullptr;
-	hr = dxCommon->GetDevice()->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
-	assert(SUCCEEDED(hr)); // うまくいかなかったら動かない
-#pragma endregion
-
-#pragma region VertxBufferViewを作成
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
-	// リソースの先頭アドレスから使う
-	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
-	// 使用するリソースのサイズは頂点三つ分のサイズ
-	vertexBufferView.SizeInBytes = sizeof(Vector4) * 3;
-	// 1つの頂点のサイズ
-	vertexBufferView.StrideInBytes = sizeof(Vector4);
-#pragma endregion
-
-#pragma region 頂点リソースにデータを書き込む
-	Vector4* vertexData = nullptr;
-	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-	vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f}; // 左下
-	vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};   // 上
-	vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};  // 右下
-	// 頂点リソースのマップを解除する
-	vertexResource->Unmap(0, nullptr);
-#pragma endregion
-
-	// メインループ
-	while (true) {
-		// エンジンの更新
-		if (KamataEngine::Update()) {
-			break;
-		}
-
-		// 描画開始
-		dxCommon->PreDraw();
-
-		// コマンドを読む
-		commandList->SetGraphicsRootSignature(rs.Get());     // rootsignatureの設定
-		commandList->SetPipelineState(graphicsPipelineState);     // PSOの設定をする
-		commandList->IASetVertexBuffers(0, 1, &vertexBufferView); // VBVの設定
-		// トロポジの設定
-		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-		// 頂点数、インデックス数、インデックスの開始位置、インデックスのオフセット
-		commandList->DrawInstanced(3, 1, 0, 0);
-
-		// 描画終了
-		dxCommon->PostDraw();
-	}
-
-	// 解放処理
-	vertexResource->Release();
-	graphicsPipelineState->Release();
-	
-
-	// エンジンの終了処理
-	KamataEngine::Finalize();
-
-	return 0;
+	pipelineState.Create(graphicsPipelineStateDesc);
 }
